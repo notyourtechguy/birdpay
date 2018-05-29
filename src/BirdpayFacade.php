84c00b685e105d2056f19b5b74462d4149d117c2
Arki Valencia II <arkivalenciaii@notyourtechguy.me>
2018-05-29 15:27:18 +0800

    Added facades and service classes

---
+++ src/BirdpayFacade.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: falc
+ * Date: 5/29/18
+ * Time: 3:23 PM
+ */
+
+namespace Notyourtechguy\Birdpay;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class BirdpayFacade extends Facade
+{
+
+}
---
+++ src/BirdpayService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: falc
+ * Date: 5/29/18
+ * Time: 3:22 PM
+ */
+
+namespace Notyourtechguy\Birdpay;
+
+
+class BirdpayService
+{
+
+}
